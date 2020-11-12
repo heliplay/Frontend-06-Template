91ba1b0002dde9e6d08161206d20fa3184072374
heliplay <heli19920206@outlook.com>
2020-11-12 18:15:42 +0800

    Week03课堂作业和学习总结

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'standard'
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  rules: {
-    semi: ['error', 'always'],
-    indent: ['warn', 4],
-    'prefer-const': ['off', {
-      // destructuring: 'any',
-      // ignoreReadBeforeAssign: false
-    }],
-    'no-unused-vars': ['warn']
-  },
-  plugins: [
-    'html'
-  ]
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'standard'
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    rules: {
+        semi: ['error', 'always'],
+        indent: ['warn', 4],
+        'no-useless-escape': 'off',
+        'prefer-const': ['off', {
+            // destructuring: 'any',
+            // ignoreReadBeforeAssign: false
+        }],
+        'no-unused-vars': ['warn']
+    },
+    plugins: [
+        'html'
+    ]
 };
